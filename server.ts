4b3122d278ce1100eef05e64f836c804a1f3dbbe
George Campbell <campbell.goe@gmail.com>
2023-12-23 06:54:14 +0000

    fixed server / as index.html

--- server.ts
+++ server.ts
@@ -2,9 +2,16 @@ const BASE_PATH = "./public";
 Bun.serve({
   port: 3001,
   async fetch(req) {
-    const filePath = BASE_PATH + new URL(req.url).pathname;
+
+    const url = new URL(req.url);
+    const filePath = BASE_PATH + url.pathname;
+    if (filePath.endsWith("/") || filePath.endsWith("/index.html"))
+       return new Response(Bun.file(BASE_PATH+"/index.html"));
     const file = Bun.file(filePath);
+    if(file){
     return new Response(file);
+    }
+    return new Response("Nothing here")
   },
   error() {
     return new Response(null, { status: 404 });
